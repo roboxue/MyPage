fa1c47579a859bf7780100d481a7ea7976d84ea5
roboxue <roboxue@roboxue-Alienware-X51.(none)>
2013-09-26 08:18:56 -0500

    Skeleton Checkpoint

--- MyPage.py
+++ MyPage.py
@@ -2,15 +2,23 @@ __author__ = 'roboxue'
 from flask import Flask, render_template
 
 app = Flask(__name__)
+app.debug = True
 
 
 @app.route('/')
 def resume():
     return render_template('resume.html')
 
+
 @app.route('/home/')
 def home():
     return render_template('home.html')
+
+
+@app.route('/about/')
+def about():
+    return render_template('about.html')
+
 
 if __name__ == "__main__":
     port = 8000
